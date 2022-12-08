27a2ad4a74d93ab07238a562fec9828799c5b083
Mirza Chilman <mirzachilman@gmail.com>
2022-12-08 13:23:43 +0700

    feat: preload and container image in movie list

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,6 +1,6 @@
 import {useNavigate} from 'react-router-dom';
-
-const MovieCard = ({poster, title, vote, overview, id, tagline}) => {
+import {Helmet} from 'react-helmet';
+const MovieCard = ({poster, title, vote, overview, id, index}) => {
 	const navigate = useNavigate();
 	const handleCardClick = () => {
 		navigate(`/detail/${id}`);
@@ -8,11 +8,14 @@ const MovieCard = ({poster, title, vote, overview, id, tagline}) => {
 
 	return (
 		<div className='px-3' onClick={handleCardClick}>
+			<Helmet>
+				{index === 0 && <link rel='preload' as='image' href={`https://image.tmdb.org/t/p/w200${poster}`} />}
+			</Helmet>
 			<div
 				className='flex flex-col w-80 h-full rounded-lg shadow-md bg-gray-700 hover:shadow-xl transition-shadow duration-300 ease-in-out'
 			>
 				<div className='p-1'>
-					<img loading='lazy' className='w-full rounded-lg object-cover' height={64} width={64} src={`https://image.tmdb.org/t/p/w200${poster}`} alt='Sunset in the mountains' />
+					<img loading='lazy' className='w-full rounded-lg object-cover' height={172.6} width={312} src={`https://image.tmdb.org/t/p/w200${poster}`} alt='Sunset in the mountains' />
 				</div>
 				<p className='text-xl font-bold p-1'>
 					{title}
