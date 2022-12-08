1663d726df627ec94165c951e6022a8d8328deb7
Mirza Chilman <mirzachilman@gmail.com>
2022-12-08 09:31:31 +0700

    feat: refine color

--- src/components/MovieSection/NowPlaying.jsx
+++ src/components/MovieSection/NowPlaying.jsx
@@ -13,9 +13,9 @@ const NowPlaying = () => {
 	}
 
 	return (
-		<div className='flex flex-col bg-white m-auto p-auto'>
+		<div className='flex flex-col m-auto p-auto'>
 			<h1
-				className='flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800'
+				className='flex py-5 lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-200'
 			>
         Now Playing
 			</h1>
@@ -23,13 +23,12 @@ const NowPlaying = () => {
 				className='flex overflow-x-scroll pb-10 hide-scroll-bar'
 			>
 				<div
-					className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10 '
+					className='flex flex-nowrap lg:ml-40 md:ml-20 ml-2'
 				>
 					{nowPlayingMovies?.results?.map(result => {
-						const {backdrop_path, id, original_title, vote_average, poster_path, overview, tagline} = result;
-						console.log({result});
+						const {backdrop_path, id, original_title, vote_average, poster_path, overview} = result;
 						return (
-							<MovieCard key={id} id={id} title={original_title} vote={vote_average} tagline={tagline} poster={backdrop_path} overview={overview} />
+							<MovieCard key={id} id={id} title={original_title} vote={vote_average} poster={backdrop_path} overview={overview} />
 						);
 					})}
 				</div>
--- src/components/MovieSection/TopRated.jsx
+++ src/components/MovieSection/TopRated.jsx
@@ -9,9 +9,9 @@ const TopRated = () => {
 	}, []);
 
 	return (
-		<div className='flex flex-col bg-white m-auto p-auto'>
+		<div className='flex flex-col m-auto p-auto'>
 			<h1
-				className='flex lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-800'
+				className='flex lg:px-20 md:px-10 px-5 lg:mx-40 md:mx-20 mx-5 font-bold text-4xl text-gray-200'
 			>
         Top Rated
 			</h1>
@@ -19,7 +19,7 @@ const TopRated = () => {
 				className='flex overflow-x-scroll pb-10 hide-scroll-bar'
 			>
 				<div
-					className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10 '
+					className='flex flex-nowrap lg:ml-40 md:ml-20 ml-2'
 				>
 					{topRatedMovies?.results?.map(result => {
 						const {backdrop_path, id, original_title, vote_average, poster_path, overview} = result;
